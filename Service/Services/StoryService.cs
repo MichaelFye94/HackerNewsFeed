300bd564be755633726cf605b81e06707bdce332
Michael Fye <fyemichael@outlook.com>
2020-07-23 12:11:17 -0400

    :fire: Remove abstract service class. Functionality moved to data layer

--- Service/Services/StoryService.cs
+++ Service/Services/StoryService.cs
@@ -1,16 +1,14 @@
-﻿using hacker_news_feed.Service.Abstract.Services;
-using hacker_news_feed.Service.Interfaces.Story;
+﻿using hacker_news_feed.Service.Interfaces.Story;
 using hacker_news_feed.Service.Models.Item;
 using System.Collections.Generic;
-using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace hacker_news_feed.Service.Services
 {
-    public class StoryService : BaseService, IStoryService
+    public class StoryService : IStoryService
     {
         private readonly IStoryData _storyData;
-        public StoryService(HttpClient httpClient, IStoryData storyData) : base(httpClient)
+        public StoryService(IStoryData storyData)
         {
             _storyData = storyData;
         }
